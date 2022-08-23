783da41d7a76270dfdf7bbc04cb2c7e9e4254eed
Swadesh Behera <itswadesh@gmail.com>
2022-08-23 13:30:22 +0530

    drag and drop

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,13 @@
 </script>
 
 <div class="flex flex-col  justify-center">
-	<div class=" grid grid-cols-5 py-4 bg-gray-100 shadow-lg">
+	<div class=" grid grid-cols-6 py-4 bg-gray-100 shadow-lg">
 		<h1><a href="/">Voxo</a></h1>
 		<a href="/wishlist">Wishlist</a>
 		<a href="/customize">Customize</a>
 		<a href="/detail">Detail</a>
 		<a href="/login">Login</a>
+		<a href="/logout">Logout</a>
 	</div>
 	<div class="h-full">
 		<slot />
