345da959b8768238d74efa57c0bdd7ecb4f640c7
Swadesh Behera <itswadesh@gmail.com>
2022-09-03 11:24:04 +0530

    login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,1 +1,51 @@
-Login Page
+<div
+	class="container flex flex-col place-items-stretch justify-center bg-black  text-white mx-auto w-1/3 border-4 rounded-xl"
+>
+	<div class="font-bold text-3xl border-bg from-black pt-14  flex font-serif  text-pink-500">
+		| Login
+	</div>
+	<div class="flex flex-col  bg-black  text-white text py-10 my-3">
+		<input
+			type
+			class="bg-black  text-white text px-5  my-4  rounded-md h-10"
+			name=""
+			id=""
+			placeholder="Username"
+		/>
+		<hr class="mx-6" />
+		<input
+			type
+			class=" bg-black  text-white text px-5 my-12 rounded-md h-10"
+			name=""
+			id=""
+			placeholder="Password"
+		/>
+		<hr class="-my-8 mx-6" />
+
+		<p class="text-gray-400 cursor-pointer py-10 hover:text-pink-500 flex justify-end px-4">
+			Forgot your Password?
+		</p>
+		<div>
+			<button class="text-lg rounded-lg mx-7 w-11/12 h-14 bg-pink-600 font-extrabold ">
+				LOG IN
+			</button>
+			<div class="flex justify-center py-10 cursor-pointer hover:text-pink-500 text-lg">
+				Or Sign In with
+			</div>
+			<div class="flex flex-col">
+				<button
+					class="bg-blue-600 text-lg rounded-lg mx-7 w-11/12  h-14 font-medium hover:bg-blue-700"
+					>FACEBOOK</button
+				>
+				<button
+					class="bg-gray-600 text-lg rounded-lg mx-7 w-11/12  h-14 font-medium my-6 hover:bg-gray-700"
+					>GOOGLE</button
+				>
+				<div class="flex flex-row items-center justify-center py-4">
+					<p class="px-2 text-gray-500">Not a member</p>
+					<p class="hover:text-pink-600">Sign Up</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
