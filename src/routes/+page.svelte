af646a8e4725af843735c9c32ef370ae3215053d
Swadesh Behera <itswadesh@gmail.com>
2022-09-07 11:39:46 +0530

    7-9-22

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <!-- <h1 class="text-3xl font-bold underline text-red-500">Hello world!</h1> -->
-
-<div>
-	<img src="1(3).jpg" alt="" />
+<div class="container flex ">
+	<div class="left bg-[#232323] " />
+	<h2 class="">Ram Ham Colorblock T-shirt</h2>
+</div>
+<div class="right flex items-end justify-end">
+	<img class="w-1/2 h-1/2" src="/1(3).jpg" alt="" />
 </div>
--- src/routes/Nav.svelte
+++ src/routes/Nav.svelte
@@ -6,8 +6,8 @@
 	<a href="/login">Login</a>
 	<a href="/logout">Logout</a>
 </div> -->
-<nav>
-	<div class="px-28 py-9 flex flex-cols-7 space-x-11">
+<nav class="flex justify-between">
+	<div class="px-28 py-9 flex flex-cols-7 space-x-11 ">
 		<div class=" font-extrabold flex cursor-pointer text-3xl">Voxo</div>
 		<div class=" py-3 cursor-pointer font-sans font-bold text-sm">HOME ˅</div>
 		<div class=" py-3 cursor-pointer font-sans font-bold text-sm ">SHOP ˅</div>
@@ -15,6 +15,15 @@
 		<div class=" py-3 cursor-pointer font-sans font-bold text-sm">VOXO PLUS ˅</div>
 		<div class=" py-3 cursor-pointer font-sans font-bold text-sm">PAGES ˅</div>
 		<div class=" py-3 cursor-pointer font-sans font-bold text-sm">BLOG ˅</div>
-		<div />
+	</div>
+	<div class="flex space-x-8  mt-11">
+		<img class="w-7 h-6" src="/search.svg" alt="" />
+		<img class="w-7 h-6" src="/Account.svg" alt="" />
+		<img class="w-7 h-6" src="/Likes.svg" alt="" />
+	</div>
+	<div
+		class="flex mt-9 bg-[#e22454] text-white inline-block w-32 h-12 p-2 items-center font-semibold font-sans rounded-sm"
+	>
+		<img class=" w-7 h-6" src="/cart.svg" alt="" />$5686.25
 	</div>
 </nav>
