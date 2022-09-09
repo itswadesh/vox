0fd63bb9ab7128ce5bd475776c012560bcead493
Ritu Berry <ritugouda28@gmail.com>
2022-09-09 20:51:58 +0530

    detail page changed

--- src/routes/detail/+page.svelte
+++ src/routes/detail/+page.svelte
@@ -1,20 +1,43 @@
-<div class="container md:mx-auto mx-4 mt-4">
-	<div class="grid gap-3 grid-cols-2 grid-rows-2">
-		<div class="bg-gray-200">
-			<img src="http://themes.pixelstrap.com/voxo/front-end/assets/images/fashion/1.jpg" alt="" />
+<div class="bg-purple-50 py-16 h-48">
+	<div class="px-40 flex items-center justify-center font-bold text-xl ">Product 4 Images</div>
+	<div class="text-gray-500 py-2 px-40  flex items-center justify-center text-sm">
+		/ Product 4 Images
+	</div>
+</div>
+<div class="container mx-auto">
+<div class="container float-left flex  md:mx-auto px-24 py-14 mx-4 mt-4">
+	<div class="grid gap-4 grid-cols-2 grid-rows-2 ">
+		<div>
+			<img
+				class="w-24 md:w-44 lg:w-64 object-contain md:object-scale-down"
+				src="http://themes.pixelstrap.com/voxo/front-end/assets/images/fashion/1.jpg"
+				alt=""
+			/>
 		</div>
-		<div class="bg-gray-200">
-			<img src="http://themes.pixelstrap.com/voxo/front-end/assets/images/fashion/2.jpg" alt="" />
+		<div>
+			<img
+				class="w-24 md:w-44 lg:w-64 object-contain md:object-scale-down"
+				src="http://themes.pixelstrap.com/voxo/front-end/assets/images/fashion/2.jpg"
+				alt=""
+			/>
 		</div>
-		<div class="bg-gray-200">
-			<img src="http://themes.pixelstrap.com/voxo/front-end/assets/images/fashion/3.jpg" alt="" />
+		<div>
+			<img
+				class="w-24 md:w-44 lg:w-64 object-contain md:object-scale-down"
+				src="http://themes.pixelstrap.com/voxo/front-end/assets/images/fashion/3.jpg"
+				alt=""
+			/>
 		</div>
-		<div class="bg-gray-200">
-			<img src="http://themes.pixelstrap.com/voxo/front-end/assets/images/fashion/4.jpg" alt="" />
+		<div>
+			<img
+				class="w-24 md:w-44 lg:w-64 object-contain md:object-scale-down"
+				src="http://themes.pixelstrap.com/voxo/front-end/assets/images/fashion/4.jpg"
+				alt=""
+			/>
 		</div>
 	</div>
-
-	<div class="bg-gray-200 p-3 justify-center flex my-4">
+	<div class="container float-right px-6 ">
+	<div class="bg-gray-200   justify-self-start flex  ">
 		<div class="flex mr-4 items-center">
 			<img
 				src="http://themes.pixelstrap.com/voxo/front-end/assets/images/gif/fire.gif"
@@ -31,4 +54,134 @@
 			44 active view this
 		</div>
 	</div>
+	<div class="text-xl ">BRAND NEW T-SHIRT</div>
+	<div>#Best Seller</div>
+	<div>In Fashion</div>
+	<div class="text-lg font-bold">$32.96</div>
+	<div class="grid grid-cols-4  ">
+		<div>color:</div>
+		<img class="w-5 md:w-10 lg:w-16" src="http://themes.pixelstrap.com/voxo/front-end/assets/images/fashion/product/front/5.jpg" alt="">
+		<img class="w-5 md:w-10 lg:w-16" src="http://themes.pixelstrap.com/voxo/front-end/assets/images/fashion/product/front/6.jpg" alt="">
+		<img  class="w-5 md:w-10 lg:w-16" src="http://themes.pixelstrap.com/voxo/front-end/assets/images/fashion/product/front/7.jpg" alt="">
+	</div>
+	<div class="grid grid-cols-4  ">
+		<h2 class=" bg-gray-200 w-fit">s</h2>
+		<h2 class=" bg-gray-200 w-fit">m</h2>
+		<h2 class=" bg-gray-200 w-fit">l</h2>
+		<h2 class=" bg-gray-200 w-fit">xl<h2>
+	</div>
+	<div class="bg-gray-100 flex px-10 py-4 my-6">Free shipping for orders above $500 USD</div>
+
+
+</div>
+</div>
+
+<div class="container ">
+	<p class=" flex text-xl px-36">Give You A Complete Account Of The System</p>
+	<p class="flex  text-gray-500 py-2 px-36">
+		Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it
+		is pain, but because occasionally circumstances occur in which toil and pain can procure him
+		some great pleasure. To take a trivial example, which of us ever undertakes laborious physical
+		exercise, except to obtain some advantage from it? But who has any right to find fault with a
+		man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain
+		that produces no resultant pleasure.
+	</p>
+	<img
+		class="w-40 md:w-60 lg:w-1/3 float-right object-contain md:object-scale-down"
+		src="http://themes.pixelstrap.com/voxo/front-end/assets/images/fashion/slider/1.jpg"
+		alt=""
+	/>
+	<p class=" w-5/6 flex text-gray-500 px-28 ">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, autem nemo? Tempora vitae
+		assumenda laudantium unde magni, soluta repudiandae quam, neque voluptate deleniti consequatur
+		laboriosam veritatis? Tempore dolor molestias voluptatum! Minima possimus, pariatur sed, quasi
+		provident dolorum unde molestias, assumenda consequuntur odit magni blanditiis obcaecati? Ea
+		corporis odit dolorem fuga, fugiat soluta consequuntur magni.
+	</p>
+	<p class=" flex text-xl py-3 px-28">Fabric:</p>
+	<p class="flex  text-gray-500 py-2 px-36">
+		Art silk is manufactured by synthetic fibres like rayon. It's light in weight and is soft on the
+		skin for comfort in summers.Art silk is manufactured by synthetic fibres like rayon. It's light
+		in weight and is soft on the skin for comfort in summers. Lorem Ipsum is simply dummy text of
+		the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+		ever since the 1500s.
+	</p>
+	<img
+		class="w-40 md:w-60 lg:w-1/2 px-28 flex py-20 hover:float-left object-contain md:object-scale-down"
+		src="http://themes.pixelstrap.com/voxo/front-end/assets/images/fashion/slider/2.jpg"
+		alt=""
+	/>
+	<p class="flex w-5/6  text-gray-500 px-28 ">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, autem nemo? Tempora vitae
+		assumenda laudantium unde magni, soluta repudiandae quam, neque voluptate deleniti consequatur
+		laboriosam veritatis? Tempore dolor molestias voluptatum! Minima possimus, pariatur sed, quasi
+		provident dolorum unde molestias, assumenda consequuntur odit magni blanditiis obcaecati? Ea
+		corporis odit dolorem fuga, fugiat soluta consequuntur magni. Lorem ipsum, dolor sit amet
+		consectetur adipisicing elit. Odio repellat numquam perspiciatis eum quis ab, sed dicta tenetur
+		fugit culpa, aut distinctio deserunt quisquam ipsam reprehenderit iure? Adipisci, optio enim?
+		Voluptates voluptatum neque id ad commodi quisquam dolorem vitae inventore quasi! Officiis
+		facere, iusto tempore atque magnam voluptas. Architecto laboriosam deleniti hic veritatis
+		nesciunt. Aut officia quasi inventore et. Debitis.
+	</p>
+	<p class="font-bold flex px-36 text-3xl py-10">Customers Also Bought These</p>
+	<div class="grid gap-6 grid-cols-4  px-36 ">
+		<div>
+			<img
+				src="http://themes.pixelstrap.com/voxo/front-end/assets/images/fashion/product/back/1.jpg"
+				alt=""
+			/>
+			<div class=" flex justify-start px-1 py-5">
+				<div class="text-lg font-bold">Slim Fit Plastic Coat</div>
+				<div class="text-lg font-bold text-pink-700">$78.00</div>
+			</div>
+		</div>
+		<div>
+			<img
+				src="http://themes.pixelstrap.com/voxo/front-end/assets/images/fashion/product/back/2.jpg"
+				alt=""
+			/>
+			<div class=" flex justify-start px-1 py-5">
+				<div class="text-lg font-bold">Blue Leather Coat</div>
+				<div class="text-lg font-bold text-pink-700">$60.00</div>
+			</div>
+		</div>
+		<div>
+			<img
+				src="http://themes.pixelstrap.com/voxo/front-end/assets/images/fashion/product/back/3.jpg"
+				alt=""
+			/>
+			<div class=" flex justify-start px-1 py-5">
+				<div class="text-lg font-bold">Military Ball Night Coat</div>
+				<div class="text-lg font-bold text-pink-700">$85.00</div>
+			</div>
+		</div>
+		<div>
+			<img
+				class="hover:float-none"
+				src="http://themes.pixelstrap.com/voxo/front-end/assets/images/fashion/product/back/4.jpg"
+				alt=""
+			/>
+			<div class=" flex justify-start px-1 py-5">
+				<div class="text-lg font-bold">Full Sleeve Yellow Coat</div>
+				<div class="text-lg font-bold text-pink-700">$69.00</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="mx-36 my-32" />
+<div class=" container float-left py-3 bg-blue-50 h-52  flex flex-col cursor-text ">
+	<h3 class=" text-3xl font-semibold px-12 py-14  ">Subscribe Our News</h3>
+	<p class=" text-gray-500 flex px-12 -my-12">
+		Subscribe and receive our newsletters to follow the news about our fresh and fantastic Products.
+	</p>
+	<div class="flex items-end justify-end -my-1 px-16">
+		<input
+			type="email"
+			name=""
+			id=""
+			class="bg-white text-brown-600 w-80 h-12 text-left pl-6"
+			placeholder="Your Email Address"
+		/><button class="bg-rose-600 text-white w-24 h-12">Button</button>
+	</div>
+</div>
 </div>
